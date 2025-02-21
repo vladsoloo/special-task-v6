d65bc306c79af8c090a927899ff85eb6cb6d1dfa
Xlxlrdyyy <solo.vlad55555@icloud.com>
2025-02-21 08:10:48 +0000

    сделал' '

---
+++ task15.py
@@ -0,0 +1,19 @@
+def print_numbers_less_than_a(a):
+    n = 1
+    while True:
+
+        current_number = 1 + 1 / n
+
+        if current_number < a:
+            print(current_number)
+        else:
+
+            break
+
+        n += 1
+
+
+a = float(input("Введите число a: "))
+
+# Вызов функции
+print_numbers_less_than_a(a)
