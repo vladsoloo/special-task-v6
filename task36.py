27382f6cc32a0aa2447502923886fe932d50b72c
Xlxlrdyyy <solo.vlad55555@icloud.com>
2025-02-27 11:16:16 +0000

    сделал

---
+++ task36.py
@@ -0,0 +1,14 @@
+first_multiple = 100 + (9 - (100 % 9))
+if first_multiple % 10 != 7:
+    first_multiple += 9
+
+
+numbers = []
+current = first_multiple
+while len(numbers) < 10:
+    if current % 10 == 7:
+        numbers.append(current)
+    current += 9
+
+print("10 первых натуральных чисел, кратных 9, оканчивающихся на 7, начиная с 100: ")
+print(numbers)
